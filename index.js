98f35fee1ead3c364ff28caa5308ba7e8bfbac22
laredojuan <lauredojuan@gmail.com>
2020-07-23 16:09:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let battelfield = document.querySelector(".container");
+
+let fireFunction = () => {
+//    battelfield.childNodes[Math.floor(Math.random() * 10)].childNodes[Math.floor(Math.random() * 10)].style.background= "red"; 
+
+let rows = battelfield.childNodes[Math.floor(Math.random() * 10)];
+let cells = rows.childNodes[Math.floor(Math.random() * 10)];
+
+cells.style.background= "red";
+
+console.log(rows);
+console.log(cells);
+}
+
+window.onload = () => {
+     for (let i=0; i<10; i++){
+        let row = document.createElement("div");
+        battelfield.appendChild(row);
+        row.style.display = "flex";
+        for (let j=0; j<10; j++){
+            let cell = document.createElement("div");
+            row.appendChild(cell);
+            cell.innerHTML = Math.floor(Math.random() * 2);
+            cell.style.border= "2px solid gray"
+            cell.style.padding= "15px"
+            if (cell.innerHTML == 1) cell.style.background= "gray";
+        }
+     }
+    let button = document.createElement("button");
+    button.innerHTML = "Fire";
+    button.style.padding = "10px 30px";
+    button.style.background = "red";
+    battelfield.appendChild(button);
+    button.addEventListener("click" , fireFunction);
+};
+
+
+
+
+
